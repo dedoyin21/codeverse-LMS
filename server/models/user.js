f6badacaee02155e5d3352353844c24f72ba12c3
sejoroajose <53483859+sejoroajose@users.noreply.github.com>
2024-10-09 16:05:54 +0100

    Updating the user model to ensure password encryption

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
 
 module.exports = (sequelize) => {
   const User = sequelize.define('User', {
@@ -18,13 +19,33 @@ module.exports = (sequelize) => {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+    },
+    gitHubId: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: true,
+    },
+    googleId: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: true,
     },
     role: {
       type: DataTypes.ENUM('student', 'instructor', 'admin'),
       defaultValue: 'student',
     },
   })
+
+  User.beforeCreate(async (user) => {
+    if (user.password) {
+      user.password = await bcrypt.hash(user.password, 10)
+    }
+  })
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+  }
 
   return User
 }
