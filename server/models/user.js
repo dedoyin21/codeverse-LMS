dd0d7d6af88cba3f8067635af1e7d22c3393d880
sejoroajose <53483859+sejoroajose@users.noreply.github.com>
2024-10-09 15:40:05 +0100

    user model creation

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('student', 'instructor', 'admin'),
+      defaultValue: 'student',
+    },
+  })
+
+  return User
+}
