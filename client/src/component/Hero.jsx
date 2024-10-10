b1ee1be9d946013795faf0021c16fb6174d866b6
Marie-Adedayo <fasanyaadedayo14@gmail.com>
2024-10-10 13:33:57 +0100

    Hero

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -4,53 +4,85 @@ import React from "react";
 function Hero() {
     return(
         <>
-            <div className="w-full h-full bg-custom-cream top-5 items-center justify-center">
-                <div className=" bg-custom-cream items-center justify-center px-4  md:gap-28 md:px-12 md:pb-8 text-black ">
-                    <div className="flex flex-col md:flex-row gap-8 px-6 md:px-6 mt-32 md:mt-28 ">
-                        <div className="flex flex-col gap-4 mt-16">
-                            <h1 className="text-custom-red text-4xl font-serif font-semibold text-left justify-center">
-                                "Empowering Techinnovators Around the World"
+            <div className="w-full h-screen bg-custom-cream top-5 items-center justify-center">
+                <div className=" bg-custom-cream items-center justify-center px-4  md:gap-8 md:mt-28 md:px-12 text-black ">
+                    <div className="flex flex-col md:flex-row gap-8 px-6 md:px-6 items-start">
+                        <div className="flex flex-col gap-4 w-1/2 mt-32 self-stretch">
+                            <h1 className="text-custom-red  text-5xl font-serif font-semibold text-left">
+                            "Empowering Techinnovators Around the World"
                             </h1>
-                            <p>Read more</p>
+                            
                             <div>
                                 <button className="px-4 py-1 bg-custom-red text-white rounded-2xl hover:bg-custom-orange transition duration-300">
                                     Get Started
                                 </button>
                             </div>
-
                         </div>
-                      {/*   <div>
+
+                        <div className="flex flex-row mt-10">
                             <img
-                                src="/pict1.jpg"
-                                alt="pict"
-                                className="scale-[0.7] text-gray-500"
+                            src="/pict-frontpage.jpg"
+                            alt="pict"
+                            className=" rounded-t-full"
                             />
-                            
-                        </div> */}
+                            <div className="self-stretch m-32 w-1/5">
+                                <img
+                                src="/group1.jpg"
+                                alt="pict2"
+                                className=" rounded-t-full"
+                                />
+                                <div>
+                                    <h1 className="text-custom-blue text-4xl font-bold">91%</h1>
+                                    <p className="w-[200px] text-custom-blue text-sm ">Student success from our online course</p>
+                                </div>
+                            </div>
+                        </div>
                         
                     </div>
+
                 </div>
-                <div className='bg-white px-4 py-10 z-10'>
-                    <div className="bg-custom-blue text-custom-yellow flex flex-row gap-4 md:gap-8 mx-auto w-[45%] py-8 justify-center items-center z-10 -mt-28 ">
-                        <div className='land'>
-                            <h1 className="text-4xl font-bold ">10K+</h1>
-                            <p>Success Stories</p>
+                <div className=''>
+                    <div className="bg-custom-cream text-black flex flex-row gap-4 md:gap-8 py-8 justify-center items-center -mt-14 z-[15]">
+
+                        <div className="block border px-4 w-[100px] bg-custom-blue rounded-xl">
+                                <img
+                                    src="/people-group-solid.svg"
+                                    alt="image"
+                                    className="w-[80px] h-[80px]"
+                                />
                         </div>
-
-                        <hr className='bg-white w-1 h-12 mx-4' />
-
-                        <div className='t-p'>
-                            <h1 className="text-4xl font-bold">15K+</h1>
-                            <p>Trained Persons</p>
+                        <div >                         
+                            <h1 className="text-4xl font-bold">150+</h1>
+                            <p className="text-gray-600">From the world's best</p>
                             
                         </div>
-
-                        <hr className='bg-white w-1 h-12 mx-4' />
-
-                        <div className='b-d'>
-                            <h1 className="text-4xl font-bold">350</h1>
-                            <p>Business Developed</p>
+                        <div className="border px-4 w-[100px] bg-custom-purple rounded-xl">
+                            <img
+                                src="/icons-screen.png"
+                                alt="image"
+                                className="w-[80px] h-[80px] "
+                            />
                         </div>
+
+                        <div className='t-p'>
+                           
+                            <h1 className="text-4xl font-bold">20 lessons</h1>
+                            <p className="text-gray-600">Avrage per class</p>
+                            
+                        </div>
+                        <div className=" border px-4 w-[100px] bg-custom-yellow rounded-xl">
+                            <img
+                               src="/clock-regular.svg"
+                               alt="image"
+                               className="w-[80px] h-[80px] "
+                            />
+                        </div>
+                            
+                        <div>
+                            <h1 className="text-4xl font-semibold">10 minutes</h1>
+                            <p className="text-gray-600">Average per lesson </p>
+                        </div>
+
                     </div>
                 </div>
             </div>
