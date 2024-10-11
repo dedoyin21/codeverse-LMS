8d2f85bf99c10f74dc4d8e9b6cd0deec614c6945
dedoyin21 <adedirandedoyin@gmail.com>
2024-10-11 01:42:57 -0700

    delete page/dboard

--- client/src/component/header.jsx
+++ client/src/component/header.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import { ThemeCotext } from '../Context/ThemeContextProvider'
+import codeverseLogo from '../../src/assets/codeverseLogo.svg'
 import AccountDemoSignedIn from "../component/Account"
 
 const Navbar = () => {
     const {theme, toggleTheme} = useContext(ThemeCotext)
   return (
     <div className='bg-custom-white text-gray-900 border-b border-gray-300 p-4 flex justify-between items-center dark:border-gray-600 dark:bg-gray-900 dark:text-white'>
-        <h1>Dashboard</h1>
+        <img src={codeverseLogo} className={`overflow-hidden transition-all`} alt="Logo" />
+
         
         <div className="flex gap-4">
           <AccountDemoSignedIn />
