7d235dae2882094767eafa8de164cc2dd09e9250
Marie-Adedayo <fasanyaadedayo14@gmail.com>
2024-10-29 15:37:14 +0100

    styling of the hero section

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -3,26 +3,42 @@ import React from "react";
 function Hero() {
     return (
         <>
-            {/* Hero Section */}
-            <div className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-b from-red-700 via-black to-black text-custom-cream">
-                <div className="text-center flex flex-col gap-4 mt-4 md:mt-28 px-6 ">
-                    {/* Bold Heading */}
-                    <h1 className="text-4xl md:text-5xl font-bold font-cabinetGrotesk">
-                    "Shape the Future of Tech by Mastering the Skills of Tomorrow"
+            <div className="w-full h-screen flex flex-col-reverse md:flex-row justify-center items-center text-custom-cream  bg-gradient-to-b from-red-500 to-black">
+                <div className="w-full md:w-1/2 flex flex-col gap-4 mt-4 lg:mt-28 px-4 md:px-10 lg:px-16">
+                    <h1 className="text-center md:text-left text-3xl md:text-4xl lg:text-5xl font-bold font-cabinetGrotesk">
+                        Shape the Future of Tech by Mastering the Skills of Tomorrow
                     </h1>
-                    {/* Normal Paragraph */}
-                    <p className="text-base md:text-xl px-14 justify-center items-center font-sans  font-light">
-                    We are revolutionizing the World of Technology; transforming Tech Newbies into Industry Game-Changers, by enhancing their skills to prepare them for the ever-evolving Digital Age.
+                    <p className="text-sm px-2 md:px-4 font-sans font-normal">
+                        We are revolutionizing the World of Technology; transforming Tech Newbies into Industry Game-Changers, by enhancing their skills to prepare them for the ever-evolving Digital Age.
                     </p>
-                    {/* Buttons */}
-                    <div className="flex flex-col gap-4 mt-6">
-                        <button className="px-4 font-sans font-normal py-2 md:px-6 md:py-3 bg-custom-red text-custom-cream text-lg md:text-xl rounded-full hover:bg-custom-cream hover:text-custom-red transition-all duration-300 w-48 mx-auto">
+                    <div className="flex flex-col md:flex-row gap-4 mt-6">
+                        <button className="px-3 py-2 md:px-5 md:py-2 bg-custom-red text-custom-cream text-base md:text-lg rounded-full hover:bg-custom-cream hover:text-custom-red transition-all duration-300 w-40 mx-auto md:mx-0">
                             Get Started
                         </button>
-                        <button className="px-4 font-sans font-normal py-2 md:px-6 text-custom-red md:py-3 bg-custom-cream  text-lg md:text-xl rounded-full hover:bg-custom-red hover:text-custom-cream transition-all duration-300 w-48 mx-auto">
+                        <button className="px-3 py-2 md:px-5 md:py-2 border-custom-red border-2 bg-custom-cream text-custom-red text-base md:text-lg rounded-full hover:bg-custom-red hover:text-custom-cream transition-all duration-300 w-40 mx-auto md:mx-0">
                             Learn More
                         </button>
                     </div>
+                    <div className="flex items-center space-x-2">
+                        <div className="relative flex -space-x-4">
+                            <img src="/student.png" alt="Student 1" className="w-10 h-10 rounded-full border-2 border-white object-cover" />
+                            <img src="/student1.png" alt="Student 2" className="w-10 h-10 rounded-full border-2 border-white object-cover" />
+                            <img src="/student3.jpg" alt="Student 3" className="w-10 h-10 rounded-full border-2 border-white object-cover" />
+                            <img src="/student4.jpg" alt="Student 4" className="w-10 h-10 rounded-full border-2 border-white object-cover" />
+                        </div>
+                        <div className="flex flex-col items-left">
+                            <p className="text-2xl font-bold">165+</p>
+                            <p className="text-sm text-gray-500">Enrolled Students</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="w-full md:w-1/2 flex justify-center mt-10 md:mt-20">
+                    <img 
+                        src="/Cartoon.svg" 
+                        alt="Illustration of tech advancements"
+                        className="w-96 md:w-96 lg:w-full"
+                    />
                 </div>
             </div>
         </>
